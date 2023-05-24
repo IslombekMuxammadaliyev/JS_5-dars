12c40c30beaad4efd82f88187aa10683de35e4a0
uzb077 <islombekmuxammadaliyev07@gmail.com>
2023-05-24 20:57:23 +0500

    Bu JavaScript kodlari

---
+++ js/main.js
@@ -0,0 +1,55 @@
+/*HTML teglari bilan ishlash*/
+
+/*
+let headTag = document.head;
+let bodyTag = document.body;
+let havola = document.documentURI
+let formalar = document.forms
+console.log("formalar")
+*/
+
+/*Event in JS (onclick,ondblclick)*/
+/*function battoniBos(){
+    alert("Siz bir marta battoni bosdingiz!")
+}
+function battoniIkkiMartaBosdi(){
+    alert("Siz ikki marta battoni bosdingiz!")
+}
+
+function tanlash(event){
+    alert("Siz" + event.target.value + "tanladingiz" )
+}
+
+function inputgaBosish(){
+    console.log("Inputga bosildi!")
+}
+
+function inputdanChiqish(){
+    console.log("Inputdan chiqildi!")
+}
+
+function yozish(event){
+    console.log(event.target.value)
+}*/
+
+/*
+document.querySelector('.classNomi').addEventListener('copy',function (event){
+    console.log("Diqqat! Sizdan '" + event.target.value + "nomli fayllar ko'chirilyapti!")
+})
+*/
+
+function harakatlanish(e){
+    console.log(e.clientX,e.clientY)
+}
+function kirish(){
+    alert("Hududga kirildi")
+}
+function chiqish(){
+    alert("Hududga chiqildi")
+}
+
+
+
+
+
+
